bcb7310638a0d983f985acb3dbd77cc9e44fea97
smallfang <smallfang@rotriw.com>
2024-09-04 21:09:02 +0800

    fix: upload url

--- src/handler/upload.rs
+++ src/handler/upload.rs
@@ -4,7 +4,7 @@ use actix_web::{post, services, web, Scope};
 /*
 直接上传数据
 */
-#[post("/upload")]
+#[post("")]
 async fn upload_user(data: web::Json<CreateUser>) -> ResultHandler<String> {
     let _ = upload_user_by_create_user(data.into_inner());
     Ok(Json! {
